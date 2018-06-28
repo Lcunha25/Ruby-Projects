2bdcfd7b37ab20afd9a6bd04c46cdf044b51303e
Lucas <lucaspereiracunha08@gmail.com>
2018-06-28 12:13:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Ruby
+This is a small project I am doing to convert currencies and see if there are any triangular arbitrage opportunities.
+
+# Structure
+The project is interacting with the XE currency conversion website. It uses Mechanize to interact with the website, inputting information to the two choosen currencies to be exchanged agains the USD and comparing triangulating it.
+
+# Running the project
+-First install locally mechanize and Ruby.
+-Type in the CLI, when in the project folder, ruby rate.rb
+-Enter one of the following currencies to compare it to the USD
+
+#  AED - Emirati Dirham, BHD - Bahraini Dinar, HKD - Hong Kong Dollar, JOD - Jordanian Dinar, JPY - Japanese Yen, KWD - Kuwaiti Dinar,
+#  OMR - Omani Rial, PHP - Philippine Piso, QAR - Qatari Riyal, SAR - Saudi Arabian Riyal, THB - Thai BahtEurope, BGN - Bulgarian Lev, 
+#  CHF - Swiss Franc, CZK - Czech Koruna, DKK - Danish Kronea, EUR - Euro, GBP - British Pound, HRK - Croatian Kuna, HUF - Hungarian Forint, 
+#  ILS - Israeli Shekel, NOK - Norwegian Krone, PLN - Polish Zloty, RON - Romanian Leu, RSD - Serbian Dinar, SEK - Swedish Krona, 
+#  TRY - Turkish Lira, CAD - Canadian Dollar, JMD - Jamaican Dollar, MXN - Mexican Peso, USD - US Dollara, AUD - Australian Dollar, 
+#  FJD - Fijian Dollar, NZD - New Zealand Dollar, BWP - Botswana Pula, KES - Kenyan Shilling, LSL - Basotho Loti, MUR - Mauritian Rupee, 
+#  SZL - Swazi Lilangeni, TND - Tunisian Dinar, ZAR - South African Rand"
+
+-Again enter the second currency you would like to compare on the cli.
+-Wait for the result, if there is an arbitrtion opportunity the system will display a message saying there is an opportunity.
+
+# Development
+-The project is still in development and the goal is to have it functioning with and web interface and communicating with other exchanges. The end result will show arbitrage opportunities between different currencies in different systems and.
---
+++ rate.rb
@@ -0,0 +1,142 @@
+#!/usr/bin/env ruby
+
+# this system will get 2 currencies parings and compare it to a third currency parying and see if there are any arbitrage opportunities:
+
+require 'mechanize'
+
+# GET'S THE FIRST CURRENCY
+
+print "What rate would you like to convert to?"
+    to_rate = gets.chomp
+
+class Xchanger
+  def self.get_url from, to
+    "http://www.xe.com/currencyconverter/convert/?Amount=1&From=#{from}&To=#{to}"
+  end
+  def self.get_rate from, to
+    a= Mechanize.new
+    pg= a.get get_url from, to
+    element = pg.search('//*[@id="ucc-container"]/span[2]/span[2]')
+    rate = element.text.to_f
+  end
+  def initialize from: "USD", to: "BRL"
+    @from = from.upcase
+    @to = to.upcase
+    update_rate
+  end
+  def convert n
+    n * @rate
+  end
+  def get_rate
+    self.class.get_rate from, to
+  end
+  def update_rate
+    @rate = get_rate
+  end
+  attr_reader :from, :to, :rate
+  def inspect
+    "<Xchanger - Rate from #{from} to #{to} is #{rate}>"
+  end
+end
+
+from_rate = "USD"
+
+if __FILE__ == $0
+  from = ARGV[0] || "#{from_rate}"
+  to = ARGV[1] || "#{to_rate}"
+  amount = 1000
+  x = Xchanger.new from: from, to: to
+  first_result = x.convert amount
+  puts "#{amount} #{from} is #{first_result} #{to}"
+  $conversion_rate = "#{first_result}"
+end
+# GETS THE SECOND CURRENCY COMPARISSION.
+
+print "What rate would you like to triangle your exchange with?"
+    to_rate2 = gets.chomp
+
+class Xchanger
+  def self.get_url from, to
+    "http://www.xe.com/currencyconverter/convert/?Amount=1&From=#{from}&To=#{to}"
+  end
+  def self.get_rate from, to
+    a= Mechanize.new
+    pg= a.get get_url from, to
+    element = pg.search('//*[@id="ucc-container"]/span[2]/span[2]')
+    rate = element.text.to_f
+  end
+  def initialize from: "USD", to: "BRL"
+    @from = from.upcase
+    @to = to.upcase
+    update_rate
+  end
+  def convert n
+    n * @rate
+  end
+  def get_rate
+    self.class.get_rate from, to
+  end
+  def update_rate
+    @rate = get_rate
+  end
+  attr_reader :from, :to, :rate
+  def inspect
+    "<Xchanger - Rate from #{from} to #{to} is #{rate}>"
+  end
+end
+
+if __FILE__ == $0
+  from = ARGV[0] || "#{from_rate}"
+  to = ARGV[1] || "#{to_rate2}"
+  amount = 1000
+  x = Xchanger.new from: from, to: to
+  second_result = x.convert amount
+  puts "#{amount} #{from} is #{second_result} #{to}"
+end
+
+# GETS THE THIRD CURRENCY COMPARISSION.
+class Xchanger
+  def self.get_url from, to
+    "http://www.xe.com/currencyconverter/convert/?Amount=1&From=#{from}&To=#{to}"
+  end
+  def self.get_rate from, to
+    a= Mechanize.new
+    pg= a.get get_url from, to
+    element = pg.search('//*[@id="ucc-container"]/span[2]/span[2]')
+    rate = element.text.to_f
+  end
+  def initialize from: "USD", to: "BRL"
+    @from = from.upcase
+    @to = to.upcase
+    update_rate
+  end
+  def convert n
+    n * @rate
+  end
+  def get_rate
+    self.class.get_rate from, to
+  end
+  def update_rate
+    @rate = get_rate
+  end
+  attr_reader :from, :to, :rate
+  def inspect
+    "<Xchanger - Rate from #{from} to #{to} is #{rate}>"
+  end
+end
+
+if __FILE__ == $0
+  from = ARGV[0] || "#{to_rate}"
+  to = ARGV[1] || "#{to_rate2}"
+  amount = first_result
+  x = Xchanger.new from: from, to: to
+  third_result = x.convert amount
+  puts "#{amount} #{from} is #{third_result} #{to}"
+end
+
+# comes up with answer for if there is an arbitrage opportunity or not:
+if second_result <= third_result
+    puts "There is an arbitrage opportunity"
+  else
+    puts "Nothing to see here"
+end
